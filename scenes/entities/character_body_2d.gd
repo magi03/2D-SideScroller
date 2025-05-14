7e079ea6c65d25f03885749aff3aa29967368b92
Магдалина Николаева <201222032@fdiba.tu-sofia.bg>
2025-05-14 22:54:43 +0200

    Added assets
    Added keys and moving platforms and enemy

--- scenes/entities/character_body_2d.gd
+++ scenes/entities/character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-@export var speed : float = 300.0
+@export var speed : float = 200.0
 @export var jump_force : float = -550.0
 @export var jump_time : float = 0.25
 @export var coyote_time : float = 0.075
